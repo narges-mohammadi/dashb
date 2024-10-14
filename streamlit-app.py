eb796ece55ffebd887728f1cfe0aebafa99c266b
narges-mohammadi <narges.mohammadi.kh@gmail.com>
2024-10-14 16:10:44 +0200

    first commit

---
+++ streamlit-app.py
@@ -0,0 +1,21 @@
+#######################
+# Import libraries
+import streamlit as st
+import pandas as pd
+import altair as alt
+import plotly.express as px
+
+#######################
+# Page configuration
+st.set_page_config(
+    page_title="World Cities Population Dashboard",
+    page_icon="🏂",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+alt.themes.enable("dark")
+
+
+#######################
+# Load data
+df = pd.read_csv('data/worldcities.csv')
