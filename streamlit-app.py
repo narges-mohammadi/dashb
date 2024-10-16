724841aebde65d5336c859c6f6de0af80c7ab945
narges-mohammadi <narges.mohammadi.kh@gmail.com>
2024-10-16 15:58:19 +0200

    modify style

--- streamlit-app.py
+++ streamlit-app.py
@@ -34,34 +34,6 @@ st.markdown("""
     padding-right: 0rem;
 }
 
-[data-testid="stMetric"] {
-    background-color: #393939;
-    text-align: center;
-    padding: 15px 0;
-}
-
-[data-testid="stMetricLabel"] {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-[data-testid="stMetricDeltaIcon-Up"] {
-    position: relative;
-    left: 38%;
-    -webkit-transform: translateX(-50%);
-    -ms-transform: translateX(-50%);
-    transform: translateX(-50%);
-}
-
-[data-testid="stMetricDeltaIcon-Down"] {
-    position: relative;
-    left: 38%;
-    -webkit-transform: translateX(-50%);
-    -ms-transform: translateX(-50%);
-    transform: translateX(-50%);
-}
-
 </style>
 """, unsafe_allow_html=True)
 
