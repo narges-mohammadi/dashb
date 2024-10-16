64b7f04fc0197ec883de9ee4d2981643ae4ee644
narges-mohammadi <narges.mohammadi.kh@gmail.com>
2024-10-16 15:13:40 +0200

    add sidebar to dashbaord

--- streamlit-app.py
+++ streamlit-app.py
@@ -16,7 +16,69 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded")
 
+#######################
+# CSS styling
+st.markdown("""
+<style>
+
+[data-testid="block-container"] {
+    padding-left: 2rem;
+    padding-right: 2rem;
+    padding-top: 1rem;
+    padding-bottom: 0rem;
+    margin-bottom: -7rem;
+}
+
+[data-testid="stVerticalBlock"] {
+    padding-left: 0rem;
+    padding-right: 0rem;
+}
+
+[data-testid="stMetric"] {
+    background-color: #393939;
+    text-align: center;
+    padding: 15px 0;
+}
+
+[data-testid="stMetricLabel"] {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+[data-testid="stMetricDeltaIcon-Up"] {
+    position: relative;
+    left: 38%;
+    -webkit-transform: translateX(-50%);
+    -ms-transform: translateX(-50%);
+    transform: translateX(-50%);
+}
+
+[data-testid="stMetricDeltaIcon-Down"] {
+    position: relative;
+    left: 38%;
+    -webkit-transform: translateX(-50%);
+    -ms-transform: translateX(-50%);
+    transform: translateX(-50%);
+}
+
+</style>
+""", unsafe_allow_html=True)
+
 
 
 # Load data
 df = pd.read_csv('./data/worldcities.csv')
+
+# Sidebar
+with st.sidebar:
+    st.title('🏂 World Cities Population Dashboard')
+    
+    country_list = list(df.country.unique())[::-1]
+    
+    selected_country = st.selectbox('Select a Country', country_list)
+    df_selected_country = df[df.country == selected_country]
+    df_selected_country_sorted = df_selected_country.sort_values(by="population", ascending=False)
+
+    color_theme_list = ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds', 'rainbow', 'turbo', 'viridis']
+    selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
