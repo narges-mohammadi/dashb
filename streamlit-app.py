ddfaed8fb6edb2f8fbb8e8568da2f7b415bc07c8
narges-mohammadi <narges.mohammadi.kh@gmail.com>
2024-10-16 14:59:09 +0200

    change to plotting dashboard version 2

--- streamlit-app.py
+++ streamlit-app.py
@@ -4,8 +4,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 #import altair as alt
-#import plotly.express as px
-#import matplotlib.pyplot as plt
+import plotly.express as px
+import matplotlib.pyplot as plt
 
 #######################
 # Page configuration
